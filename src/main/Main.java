1dd2dd8af344afee6ee0bcdfdfd774030a487787
Makomo627 <lekhahuy9@gmail.com>
2026-08-30 17:21:05 +0700

    feat(main): initialize Main entry point and application flow

---
+++ src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+import view.MainMenuView;
+public class Main {
+    public static void main(String[] args) {
+        MainMenuView mainMenuView = new MainMenuView();
+        mainMenuView.start();
+    }
+}
